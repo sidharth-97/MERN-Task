470e0aee364bc363907a045e6b9830e41c7df4e3
Sidharth PT <sidharthanand73@gmail.com>
2024-03-13 14:27:19 +0530

    feat: Created all APIs

--- server/routes/routes.ts
+++ server/routes/routes.ts
@@ -1,10 +1,14 @@
 import express from "express"
-import { allTransactions, initializeDb } from "../controller/controller"
+import { Statistics, allTransactions, categoryList, fetchAll, initializeDb, priceRange } from "../controller/controller"
 
 const router = express.Router()
 
 router.post("/initializeDb", initializeDb)
-router.get("/alltransactions",allTransactions)
+router.get("/alltransactions", allTransactions)
+router.get("/statistics", Statistics)
+router.get("/pricerange", priceRange)
+router.get("/categorylist", categoryList)
+router.get("/fetchall",fetchAll)
 
 
 export default router
