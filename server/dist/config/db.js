d48f62d0a461bc59b0a19bfe9edeb87968c5a3e4
Sidharth PT <sidharthanand73@gmail.com>
2024-03-14 12:03:54 +0530

    fix:package.json for deployment

--- server/dist/config/db.js
+++ server/dist/config/db.js
@@ -1,3 +1,4 @@
+"use strict";
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -7,15 +8,19 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-import mongoose from "mongoose";
-const connectDB = () => __awaiter(void 0, void 0, void 0, function* () {
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = __importDefault(require("mongoose"));
+const ConnectDB = () => __awaiter(void 0, void 0, void 0, function* () {
     try {
-        const connect = yield mongoose.connect(process.env.MONGO_URI);
-        connect && console.log("MongoDB connected");
+        const connect = yield mongoose_1.default.connect(process.env.MONGO_URI);
+        console.log("MongoDB connected");
     }
     catch (error) {
-        console.log(`Error--${error.message}`);
+        console.log(`Error--${error}`);
         process.exit(1);
     }
 });
-export default connectDB;
+exports.default = ConnectDB;
--- server/dist/index.js
+++ server/dist/index.js
@@ -3,11 +3,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
 const express_1 = __importDefault(require("express"));
+const db_1 = __importDefault(require("./config/db"));
+const routes_1 = __importDefault(require("./routes/routes"));
+(0, db_1.default)();
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.get("/", (req, res) => {
-    res.send("test server");
-});
+app.use("/api", routes_1.default);
 app.listen(3000, () => console.log("Server connected"));
